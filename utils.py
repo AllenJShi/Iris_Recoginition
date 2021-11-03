b57399f04a23270f68de2fc0a0c92ee1e06976e7
AllenJShi <allenjunsheng@gmail.com>
2021-11-03 06:05:40 -0400

    updated graph

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
-import numpy as np
-import cv2
-import os
 import glob
+import cv2
+import numpy as np
+from IrisLocalization import *
+from IrisNormalization import *
+from ImageEnhancement import *
+from FeatureExtraction import *
+
 
 directory = './data/'
 
@@ -34,7 +38,78 @@ def readDataset(train=True,test=False):
     return (train_img, test_img) if (train and test) else (train_img if train else test_img)
 
 
+def rotateImg(img,offset):
+    pixels = abs(int(512*offset/360))
+    if offset > 0:
+        return np.hstack([img[:,pixels:],img[:,:pixels]] )
+    else:
+        return np.hstack([img[:,(512 - pixels):],img[:,:(512 - pixels)]])
+
+
+
+def saveNormalizedImage():
+    train = np.load("train.npy")
+    norm = []
+    for i,img in enumerate(train):
+        inner_circle, outer_circle = irisLocalization(img)
+        # img = denoising(img)
+        img_norm = irisNormalization(img,inner_circle,outer_circle)
+        img_enhance = imageEnhancement(img_norm)
+        norm.append(img_enhance)
+        print(f"process class {int(i/3)}: {i}/{len(train)}")
+    np.save("train_norm",norm)
+
+
+def rotateAll():
+    norm = np.load("train_norm.npy")
+    offsets = [-9,-6,-3,0,3,6,9]
+    res = []
+    for i, img in enumerate(norm):
+        for offset in offsets:
+            p = rotateImg(img,offset)
+            res.append(p)
+        print(f"process class {int(i/3)}:{i}/{len(norm)}")
+    np.save("train_norm_rotate.npy",res)
+    
+    
+def extractFeatureFromRotatedImg():
+    imgs = np.load("train_norm_rotate.npy")
+    V = []
+    for i,img in enumerate(imgs):
+        feature_vector = featureExtraction(img)
+        V.append([feature_vector])
+        print(f"process class {int(i/21)}:{i}/{len(imgs)}")
+    np.save("X_train_rotate",V)
+
+
+# denoising decreased the accuracy 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# def denoisingAll():
+#     imgs = np.load("train.npy")
+#     denoise = []
+#     for i,img in enumerate(imgs):
+#         (_, B) = cv2.threshold(img ,180 ,255 ,cv2.THRESH_BINARY)
+#         (_, C) = cv2.threshold(img ,100 ,255 ,cv2.THRESH_BINARY)
+#         img = img & ~B & C
+#         denoise.append(img)
+#         print(f"process class {int(i/21)}:{i}/{len(imgs)}")
+#     np.save("train_denoise",denoise)
+    
+# def denoising(img):
+#     (_, B) = cv2.threshold(img ,180 ,255 ,cv2.THRESH_BINARY)
+#     (_, C) = cv2.threshold(img ,100 ,255 ,cv2.THRESH_BINARY)
+#     img = img & ~B & C
+#     return img
 
 if __name__=="__main__":
-    train,test = readDataset(True,True)
-    print(len(train),len(test))
+    # train,test = readDataset(True,True)
+    # print(len(train),len(test))
+    
+    saveNormalizedImage()
+    rotateAll()
+    extractFeatureFromRotatedImg()
+    
+    # imgs = np.load("X_train_rotate.npy")
+    # print(imgs.shape)
+    # denoising()
+    pass
