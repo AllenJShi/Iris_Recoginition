fixed normalization problem

ImageEnhancement.py
@@ -16,24 +16,23 @@ def imageEnhancement(img):
     cimg = img.copy()
     background = meanFilter(cimg)
     removed = removeBackground(img,background)
-    img_hist = enhanceIllumination(removed).astype(np.uint8)
+    img_hist = enhanceIllumination(removed)
     # return cv2.equalizeHist(removed.astype(np.uint8))
     return img_hist
 
 
 def meanFilter(img:np.ndarray,size=16):
     nrow,ncol = tuple(map(lambda x: int(x/size), img.shape))
-    # print(nrow,ncol)
     background = np.zeros((nrow,ncol))
     for row in range(nrow):
         for col in range(ncol):
-            value = np.mean(img[row*size: (row+1) * size,col*size: (col +1) * size])
+            value = np.mean(img[row*size: (row+1) * size,col*size: (col +1) * size],dtype=np.float32)
             background[row,col] = value
     
-    background = cv2.resize(background,None,fx=size, fy=size, interpolation = cv2.INTER_CUBIC)
+    background = cv2.resize(background,(img.shape[1],img.shape[0]), interpolation = cv2.INTER_CUBIC)
     # _,ax = plt.subplots(1,2)
-    # ax[0].imshow(background)
-    # ax[1].imshow(img)
+    # ax[0].imshow(background,cmap="gray")
+    # ax[1].imshow(img,cmap="gray")
     # plt.show()
     # print(background.shape)
     return background
@@ -46,16 +45,15 @@ def enhanceIllumination(img,size=32):
         for col in range(ncol):
             img_hist[row*size: (row+1) * size,col*size: (col +1) * size] \
                 = equalizeHelper(img[row*size: (row+1) * size,col*size: (col +1) * size])
-    return img_hist
+    img_hist = cv2.GaussianBlur(img_hist, (3, 3), 0)
+    return img_hist.astype(np.uint8)
 
 
 def removeBackground(img,background):
-    return img-background
+    enhance = img-background
+    enhance = enhance - np.amin(enhance.ravel())
+    return enhance.astype(np.uint8)  
 
 def equalizeHelper(img):
     return cv2.equalizeHist(np.array(img,dtype=np.uint8))
     
-
-
-if __name__=="__main__":
-    meanFilter(np.array([[1,2]]))

IrisNormalization.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import pi,sin,cos,floor
+import math
 
 """Refer to [https://journals.sagepub.com/doi/pdf/10.1177/1729881417703931]
 """
@@ -16,8 +17,8 @@ def irisNormalization(img:np.ndarray,inner_circle:np.ndarray,outer_circle:np.nda
         [xp_,yp_,rp] = inner_circle.astype(int).flatten()[:3]
         [xi_,yi_,ri] = outer_circle.astype(int).flatten()[:3]
     
-    for Y in range(M):
-        for X in range(N):
+    for X in range(N):
+        for Y in range(M):
             theta = 2*pi*(X/N) + offset
             if theta > 2*pi:
                 theta -= 2*pi
@@ -34,10 +35,41 @@ def irisNormalization(img:np.ndarray,inner_circle:np.ndarray,outer_circle:np.nda
     return img_normalized
 
 
+# def irisNormalization(img, inner_circle:np.ndarray,outer_circle:np.ndarray,offset=0):
+#     M, N = 64, 512
+#     # create a placeholder for normalized image
+#     img_normalized = np.zeros((M, N))
+#     # xc_inner, yc_inner, rp, xc_outer, yc_outer, ri
+    
+#     try:
+#         [xc_inner, yc_inner, rp] = inner_circle.astype(int).flatten()
+#         [xc_outer, yc_outer, ri] = outer_circle.astype(int).flatten()
+#     except:
+#         [xc_inner, yc_inner, rp] = inner_circle.astype(int).flatten()[:3]
+#         [xc_outer, yc_outer, ri] = outer_circle.astype(int).flatten()[:3]
+
+
+#     for X in range(N):
+#         for Y in range(M):
+#             theta = 2 * math.pi * (X / N)
+
+#             xp = xc_inner - rp * np.cos(theta)
+#             yp = yc_inner + rp * np.sin(theta)
+
+#             xi = xc_outer - ri * np.cos(theta)
+#             yi = yc_outer + ri * np.sin(theta)
+
+#             x = math.floor(xp + ((xi - xp)) * (Y / M))
+#             y = math.floor(yp + ((yi - yp)) * (Y / M))
+#             img_normalized[Y, X] = img[y, x]
+    
+#     return img_normalized
+
+
 def unwrap(x,y,r,theta):
     # this method normalizes irises of different size to the same size.
     # Similar to this scheme, we counterclockwise unwrap the iris
     # ring to a rectangular block with a fixed size
-    x += int(r*cos(theta))
-    y += int(r*sin(theta))
+    x += r*cos(theta)
+    y += r*sin(theta)
     return x,y

IrisRecognition.py
@@ -12,22 +12,22 @@ import random
 def test():
     subject = random.randint(0,108*3-1)
     print(subject)
-    # train,test= readDataset(train=True,test=True)   
+    train,test= readDataset(train=True,test=True)   
     # np.save("train",train)
     # np.save("test",test)
     
     
-    train = np.load("train.npy")
-    test = np.load("test.npy")
+    # train = np.load("train.npy")
+    # test = np.load("test.npy")
     
-    img = train[subject]
-    # img = train[71]
-    # 81 55 10 94
+    # img = train[subject]
+    img = train[104]
+    # 81 55 10 94 71
     # print(img.shape)
     inner_circle, outer_circle = irisLocalization(img)
     img_norm = irisNormalization(img,inner_circle,outer_circle)
     img_enhance = imageEnhancement(img_norm)
-    plt.imshow(img_enhance)
+    plt.imshow(img_enhance,cmap="gray")
     plt.show()
     
     feature_vect = featureExtraction(img_enhance)
@@ -39,10 +39,10 @@ def test():
 if __name__=="__main__":
     # test()
 
-    # train = np.load("train.npy")
-    # test = np.load("test.npy")
-    # X_train,y_train, X_test, y_test= \
-    #     irisMatching(train, test, n_components = 107,rotate=False, dimReduce = False)
+    train = np.load("train.npy")
+    test = np.load("test.npy")
+    X_train,y_train, X_test, y_test= \
+        irisMatching(train, test, n_components = 107,rotate=False, dimReduce = False)
     
     X_train = np.load("X_train.npy")
     X_test = np.load("X_test.npy")